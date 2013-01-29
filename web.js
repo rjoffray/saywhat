341f8ad94a5130bcf8b8670c1f1428045bb2762b
Richard Joffray <richard@joffray.com>
2013-01-29 13:27:10 -0800

    Added Hello World Sample Code

--- web.js
+++ web.js
@@ -1,1 +1,23 @@
-    
+var express = require('express');
+var request = require('request');
+
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
+  //response.send('Hello ');
+
+
+   var redis  = require('redis-url').connect(process.env.REDISTOGO_URL);
+   redis.set('firstname', 'Richard');
+   redis.get('firstname', function(e,v){
+       response.send("Hello "+v);
+   });
+
+});
+
+
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});    
