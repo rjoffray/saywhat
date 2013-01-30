5e7e43dace5b66ccd113e214900b99ab144e1fff
Richard Joffray <richard@joffray.com>
2013-01-30 12:34:23 -0800

    Revert "Revert "Revert "Revert "Revert "Update web.js"""""
    This reverts commit 61a4d5c196de5d4fe1564e95157a9858c08e3447.

--- web.js
+++ web.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var request = require('request');
-var crypt = require('crypto');
 
 var app = express.createServer(express.logger());
 
@@ -8,18 +7,11 @@ app.get('/', function(request, response) {
   //response.send('Hello ');
 
 
-   var secret = crypto.randomBytes(16)
-  , source = crypto.randomBytes(16)
-  , cipher = crypto.createCipheriv("aes128", secret, secret); // or createCipher
-  , decipher = crypto.createDecipheriv("aes128", secret, secret);
-
-  cipher.setAutoPadding(false);
-  decipher.setAutoPadding(false);
-
-  var step = cipher.update(source);
-  var end = decipher.update(step);
-
-  assert.strictEqual(source.toString('binary'), end); // does not fail
+   var redis  = require('redis-url').connect(process.env.REDISTOGO_URL);
+   redis.set('firstname', 'Richard');
+   redis.get('firstname', function(e,v){
+       response.send("Hello "+v);
+   });
 
 });
 
