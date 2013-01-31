1e48901306d4fdd9e409f48c169c35b07598109a
OpenShift guest <richard@joffray.com>
2013-01-30 23:26:52 -0500

    added mysql

--- web.js
+++ web.js
@@ -4,15 +4,31 @@ var request = require('request');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  //response.send('Hello ');
-
-
-   var redis  = require('redis-url').connect(process.env.REDISTOGO_URL);
-   redis.set('firstname', 'Richard');
-   redis.get('firstname', function(e,v){
-       response.send("Hello "+v);
-   });
-
+    //response.send('Hello ');
+    
+    
+//    var redis  = require('redis-url').connect(process.env.REDISTOGO_URL);
+//    redis.set('firstname', 'Richard');
+//    redis.get('firstname', function(e,v){
+//       response.send("Hello "+v);
+//    });
+    var mysql      = require('mysql');
+    var connection = mysql.createConnection({
+      host     : '216.145.5.210',
+      user     : 'vdb',
+      password : 'Password1',
+      database : 'virtuoso_technology'
+    });
+    
+    connection.connect();
+    
+    connection.query('SELECT * from dailey_quotes', function(err, rows, fields) {
+      if (err) throw err;
+    
+      console.log(rows);
+    });
+    
+    connection.end();
 });
 
 
