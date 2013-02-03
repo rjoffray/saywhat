7c8cc23cf333fa1721e107bf18ec0e8f1007fcb7
OpenShift guest <richard@joffray.com>
2013-02-03 11:43:53 -0500

    fixed key iv encoding

--- crypto-test.js
+++ crypto-test.js
@@ -1,224 +1,56 @@
 // Module dependencies
-var http = require('http'),
-    express = require('express'),
-    crypto = require('crypto');
+var express = require('express'),
+    crypto = require('crypto'),
+    assert = require('assert'),
+    app = express.createServer(express.logger());
 
 
 
+app.get('/', function(req, res) {
+    
 
-var Base64 = {
-
-// private property
-_keyStr : "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
-
-// public method for encoding
-encode : function (input) {
-    var output = "";
-    var chr1, chr2, chr3, enc1, enc2, enc3, enc4;
-    var i = 0;
-
-    input = Base64._utf8_encode(input);
-
-    while (i < input.length) {
-
-        chr1 = input.charCodeAt(i++);
-        chr2 = input.charCodeAt(i++);
-        chr3 = input.charCodeAt(i++);
-
-        enc1 = chr1 >> 2;
-        enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
-        enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
-        enc4 = chr3 & 63;
-
-        if (isNaN(chr2)) {
-            enc3 = enc4 = 64;
-        } else if (isNaN(chr3)) {
-            enc4 = 64;
-        }
-
-        output = output +
-        this._keyStr.charAt(enc1) + this._keyStr.charAt(enc2) +
-        this._keyStr.charAt(enc3) + this._keyStr.charAt(enc4);
-
-    }
-
-    return output;
-},
-
-// public method for decoding
-decode : function (input) {
-    var output = "";
-    var chr1, chr2, chr3;
-    var enc1, enc2, enc3, enc4;
-    var i = 0;
-
-    input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
-
-    while (i < input.length) {
-
-        enc1 = this._keyStr.indexOf(input.charAt(i++));
-        enc2 = this._keyStr.indexOf(input.charAt(i++));
-        enc3 = this._keyStr.indexOf(input.charAt(i++));
-        enc4 = this._keyStr.indexOf(input.charAt(i++));
-
-        chr1 = (enc1 << 2) | (enc2 >> 4);
-        chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
-        chr3 = ((enc3 & 3) << 6) | enc4;
-
-        output = output + String.fromCharCode(chr1);
-
-        if (enc3 != 64) {
-            output = output + String.fromCharCode(chr2);
-        }
-        if (enc4 != 64) {
-            output = output + String.fromCharCode(chr3);
-        }
-
-    }
-
-    output = Base64._utf8_decode(output);
-
-    return output;
-
-},
-
-// private method for UTF-8 encoding
-_utf8_encode : function (string) {
-    string = string.replace(/\r\n/g,"\n");
-    var utftext = "";
-
-    for (var n = 0; n < string.length; n++) {
-
-        var c = string.charCodeAt(n);
-
-        if (c < 128) {
-            utftext += String.fromCharCode(c);
-        }
-        else if((c > 127) && (c < 2048)) {
-            utftext += String.fromCharCode((c >> 6) | 192);
-            utftext += String.fromCharCode((c & 63) | 128);
-        }
-        else {
-            utftext += String.fromCharCode((c >> 12) | 224);
-            utftext += String.fromCharCode(((c >> 6) & 63) | 128);
-            utftext += String.fromCharCode((c & 63) | 128);
-        }
-
-    }
-
-    return utftext;
-},
-
-// private method for UTF-8 decoding
-_utf8_decode : function (utftext) {
-    var string = "";
-    var i = 0;
-    var c = c1 = c2 = 0;
-
-    while ( i < utftext.length ) {
-
-        c = utftext.charCodeAt(i);
-
-        if (c < 128) {
-            string += String.fromCharCode(c);
-            i++;
-        }
-        else if((c > 191) && (c < 224)) {
-            c2 = utftext.charCodeAt(i+1);
-            string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
-            i += 2;
-        }
-        else {
-            c2 = utftext.charCodeAt(i+1);
-            c3 = utftext.charCodeAt(i+2);
-            string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
-            i += 3;
-        }
-
-    }
-
-    return string;
-}
-
-};
+    
+    /* ***************************************************
+    rjoffray@saywhat:~/393971 (master) $ openssl enc -aes256 -k "encoding secret goes here" -P -md sha1 -a         
+    salt=ED8C48E7D528D3B2                                                                                          
+    key=0FF6DD60AED3434B5105BCAC5B2A6C96BC4F6B45FC67E98C4F98B3712CEA7B09                                           
+    iv =F00562191C6F24A5933AB9A9B0EB06AA
+    *******************************************************/
+    
 
 
+    var iv = new Buffer('8AViGRxvJKWTOrmpsOsGqg==', 'base64');
+    var key = new Buffer("D/bdYK7TQ0tRBbysWypslrxPa0X8Z+mMT5izcSzqewk=", 'base64');
+   
+    var algo = "aes256";
+    var plaintext   = "Damn you are good!!!!";
 
 
+    var cipher = crypto.createCipheriv(algo, key.toString('binary'), iv.toString('binary'));
+    var ciph = cipher.update(plaintext, 'utf8', 'base64');
+    ciph += cipher.final('base64');
+    
+    var decipher = crypto.createDecipheriv(algo, key.toString('binary'), iv.toString('binary'));
+    var txt = decipher.update(ciph, 'base64', 'utf8');
+    txt += decipher.final('utf8');
 
+    assert.equal(txt, plaintext, 'encryption and decryption with key and iv');
+    
+    var out  = "iv-base64: "+ iv.toString('base64')+"<br />";
+        out += "key-base64: "+ key.toString('base64')+"<br />";
+        out += "iv-hex: "+ iv.toString('hex')+"<br />";
+        out += "key-hex: "+ key.toString('hex')+"<br />";
+        out += "original: "+plaintext+"<br />";
+        out += "encoded: "+ciph+"<br />";
+        out += "decoded: "+txt+"<br />"+"<br />";
+        
+        res.writeHeader(200, {"Content-Type": "text/html"});  
+        res.write(out);  
+        res.end();
   
-var app = module.exports = express.createServer();
-    //blackoutAPI = http.createClient(80, 'http://api.blackoutrugby.com');
+  
 
-// Configuration
-app.configure(function(){
-    app.set('views', __dirname + '/views');
-    app.set('view engine', 'ejs');
-    app.use(express.bodyParser());
-    app.use(express.methodOverride());
-    app.use(app.router);
-    app.use(express.static(__dirname + '/public'));
 });
-
-app.configure('development', function(){
-    app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
-});
-
-app.configure('production', function(){
-    app.use(express.errorHandler()); 
-});
-
-app.use(express.bodyParser());
-
-// Routes
-var iv = "v4645jS75BWw8PjJmRmXYQ==";
-var key = "LpAHsjmXvzsj7h84UEgRYoXV/U4viJClViRvfOOc/k8=";
-app.get('/', function(req, res){
-    var iv = "v4645jS75BWw8PjJmRmXYQ==";
-    var key = "LpAHsjmXvzsj7h84UEgRYoXV/U4viJClViRvfOOc/k8=";
-    console.log(key);
-    console.log(iv);
-});
-app.get('/binary/:message', function(req, res){
-  var result = cryptoTest(req.params.message, key, iv, 'binary');
-	res.send(req.params.message + 
-		' <br/><br/> ' + 
-		result.encrypted + 
-		' <br/><br/> ' + 
-		result.decrypted + 
-		' <br/><br/> ' + 
-		'<a href="http://api.blackoutrugby.com/?d=19&er=' + result.encrypted + '">http://api.blackoutrugby.com/?d=19&er=' + result.encrypted + '</a>');
-});
-
-app.get('/base64/:message', function(req, res){
-	var result = cryptoTest(req.params.message, 'GO8FzK17iPYKE2Kt', 'E2I51NEwsC3RdSNl', 'base64');
-	res.send(req.params.message + 
-		' <br/><br/> ' + 
-		result.encrypted + 
-		' <br/><br/> ' + 
-		result.decrypted + 
-		' <br/><br/> ' + 
-		'<a href="http://api.blackoutrugby.com/?d=19&er=' + result.encrypted + '">http://api.blackoutrugby.com/?d=19&er=' + result.encrypted + '</a>');
-});
-
-function cryptoTest(data, key, iv, format) {
-	var cipher = crypto.createCipheriv('aes-192-cbc', key, iv);
-	var cipherChunks = [];
-	cipherChunks.push(cipher.update(data, 'utf8', format));
-	cipherChunks.push(cipher.final());
-
-	var decipher = crypto.createDecipheriv('aes-192-cbc', key, iv);
-	var plainChunks = [];
-	for (var i = 0;i < cipherChunks.length;i++) {
-	  plainChunks.push(decipher.update(cipherChunks[i], format, 'utf8'));
-	}
-	plainChunks.push(decipher.final());
-
-	return {
-		"encrypted": Base64.encode(cipherChunks.join('')),
-		"decrypted": plainChunks.join('')
-	};
-}
 
 // Only listen on $ node app.js
 var port = process.env.PORT || 8080;
