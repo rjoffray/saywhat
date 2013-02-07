b176d0bbc185fcb5f918f199f494e140b835387b
OpenShift guest <richard@joffray.com>
2013-02-07 11:15:29 -0500

    backup

--- crypto-test.js
+++ crypto-test.js
@@ -21,7 +21,7 @@ app.get('/', function(req, res) {
 
     var iv = new Buffer('8AViGRxvJKWTOrmpsOsGqg==', 'base64');
     var key = new Buffer("D/bdYK7TQ0tRBbysWypslrxPa0X8Z+mMT5izcSzqewk=", 'base64');
-   
+    
     var algo = "aes256";
     var plaintext   = "Damn you are good!!!!";
 
@@ -29,7 +29,7 @@ app.get('/', function(req, res) {
     var cipher = crypto.createCipheriv(algo, key.toString('binary'), iv.toString('binary'));
     var ciph = cipher.update(plaintext, 'utf8', 'base64');
     ciph += cipher.final('base64');
-    
+    //var ciph = new Buffer("q770t-Cein5Knn3FL6skn3yhWQNKQpy7wcjNbGLzgn5qKNOTMUe8VQ==", 'base64');
     var decipher = crypto.createDecipheriv(algo, key.toString('binary'), iv.toString('binary'));
     var txt = decipher.update(ciph, 'base64', 'utf8');
     txt += decipher.final('utf8');
@@ -43,6 +43,11 @@ app.get('/', function(req, res) {
         out += "original: "+plaintext+"<br />";
         out += "encoded: "+ciph+"<br />";
         out += "decoded: "+txt+"<br />"+"<br />";
+        
+    var stext = new Buffer('152/XnbdFqiy5YGldi-HnN5y7nW2xkMrYGFAO7ao4uWXYOKlWPx4gkDj4GpBfl1aNHt5WkRJibcR6ShxJRCyhysFteLeASQNJX8RQe7FYy0YhHk_7nLv1X4tMuTgrJHbJOsiQZerigPlOlIIUa0KMIlQPA==','base64');
+    var sdecipher = crypto.createDecipher(algo, key.toString('binary'), iv.toString('binary'));
+    var stxt = sdecipher.update(stext, 'base64', 'utf8');
+    out += "secret: "+stxt+"<br />"+"<br />";
         
         res.writeHeader(200, {"Content-Type": "text/html"});  
         res.write(out);  
